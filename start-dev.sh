41a11ad55504ca0e7b1e2f895fd6739de9578355
myx777 <anton.kruglov89@gmail.com>
2024-12-26 00:40:47 +0300

    correct script

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Запуск Docker Compose
+docker compose -f docker-compose.dev.yml -p hotel up -d
+
+# Дождаться инициализации MongoDB
+echo "Waiting for MongoDB to finish initialization..."
+MAX_RETRIES=20  # Таймаут в секундах
+RETRY_COUNT=0
+
+while ! docker exec mongodb-dev mongosh --eval "db.stats()" >/dev/null 2>&1; do
+    if [ "$RETRY_COUNT" -ge "$MAX_RETRIES" ]; then
+        echo "Error: MongoDB initialization timed out. Exiting."
+        exit 1
+    fi
+    echo "MongoDB is still initializing... $(($MAX_RETRIES - $RETRY_COUNT)) seconds remaining"
+    RETRY_COUNT=$((RETRY_COUNT + 1))
+    sleep 1
+done
+echo "MongoDB is initialized!"
+
 # Загружаем переменные из .env
 set -o allexport
 source .env
@@ -10,17 +29,6 @@ if [ -z "$MONGO_HOTELS_WRITE_USER" ] || [ -z "$MONGO_HOTELS_WRITE_PASSWORD" ] ||
     echo "Error: One or more required environment variables are missing. Check your .env file."
     exit 1
 fi
-
-# Запуск Docker Compose
-docker compose -f docker-compose.dev.yml -p hotel up -d
-
-# Дождаться инициализации MongoDB
-echo "Waiting for MongoDB to finish initialization..."
-until docker exec mongodb-dev mongosh --eval "db.stats()" >/dev/null 2>&1; do
-    echo "MongoDB is still initializing..."
-    sleep 1
-done
-echo "MongoDB is initialized!"
 
 # Обработка файла mongo-init.js и подстановка паролей и юзеров из .env
 echo "Processing mongo-init.js with environment variables..."
